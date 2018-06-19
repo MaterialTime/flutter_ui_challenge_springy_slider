7725dbced964e8ebd92beb68272023a34e5b5789
Matt Carroll <me@mattcarroll.name>
2018-06-18 22:54:52 -0700

    1. App Bars

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Springy Slider',
       theme: new ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFFFF6688),
       ),
       home: new MyHomePage(),
     );
@@ -21,11 +21,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildTextButton(String title, bool isOnLight) {
+    return FlatButton(
+      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Text(title,
+          style: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.bold,
+            color: isOnLight ? Theme.of(context).primaryColor : Colors.white,
+          )),
+      onPressed: () {
+        // TODO:
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Container(),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+          ),
+          onPressed: () {
+            // TODO:
+          },
+        ),
+        actions: [
+          _buildTextButton('settings'.toUpperCase(), true),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          new Container(
+            color: Theme.of(context).primaryColor,
+            child: Row(
+              children: <Widget>[
+                _buildTextButton('more'.toUpperCase(), false),
+                new Expanded(child: new Container()),
+                _buildTextButton('stats'.toUpperCase(), false),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
